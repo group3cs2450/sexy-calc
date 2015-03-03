6753e8de7c51d911738d743e76f76f18b5cb9e9b
Cody Brown <codybrowncit@gmail.com>
2015-03-03 13:09:18 -0700

    fixed broken server

--- sexy_calculator/db/schema.rb
+++ sexy_calculator/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150221163914) do
+ActiveRecord::Schema.define(version: 20150303200030) do
 
   create_table "orders", force: true do |t|
     t.integer  "width"
@@ -26,7 +26,6 @@ ActiveRecord::Schema.define(version: 20150221163914) do
     t.integer  "aisles"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
